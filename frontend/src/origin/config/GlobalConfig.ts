313a16edb539d48b51538dbc8a4340f95ac511bf
Daro <dario.palminio@gmail.com>
2021-06-20 19:38:50 -0400

    Rename environment variables

--- frontend/src/origin/config/GlobalConfig.ts
+++ frontend/src/origin/config/GlobalConfig.ts
@@ -1,24 +1,22 @@
 
 require('dotenv').config();
 
-// REACT_APP_ENV: prod | dev | qa
-export const environment = process.env.REACT_APP_ENV
+// CLIENT_WEB_APP_ENV: prod | dev | qa
+export const environment = process.env.CLIENT_WEB_APP_ENV
 
 export const APIEndpoints = {
-  AUTH: process.env.REACT_APP_AUTH_API,
-  NOTIFICATION: process.env.REACT_APP_NOTIFICATION_API,
+  AUTH: process.env.CLIENT_WEB_APP_AUTH_API,
+  NOTIFICATION: process.env.CLIENT_WEB_APP_NOTIFICATION_API,
 }
 
 export const Keycloak = {
-  REALM: process.env.REACT_APP_AUTH_REALM as string,
-  CLIENT_ID: process.env.REACT_APP_AUTH_CLIENT_ID as string,
-  CLIENT_SECRET: process.env.REACT_APP_AUTH_CLIENT_SECRET as string,
-  USERNAME_ADMIN: process.env.REACT_APP_AUTH_USERNAME_ADMIN as string,
-  PASSWORD_ADMIN: process.env.REACT_APP_AUTH_PASSWORD_ADMIN as string,
-  VERIFY_EMAIL: ((process.env.REACT_APP_AUTH_VERIFY_EMAIL === 'true') ? true : false) as boolean
+  REALM: process.env.CLIENT_WEB_APP_AUTH_REALM as string,
+  CLIENT_ID: process.env.CLIENT_WEB_APP_AUTH_CLIENT_ID as string,
+  CLIENT_SECRET: process.env.CLIENT_WEB_APP_AUTH_CLIENT_SECRET as string,
+  USERNAME_ADMIN: process.env.CLIENT_WEB_APP_AUTH_USERNAME_ADMIN as string,
+  PASSWORD_ADMIN: process.env.CLIENT_WEB_APP_AUTH_PASSWORD_ADMIN as string,
+  VERIFY_EMAIL: ((process.env.CLIENT_WEB_APP_AUTH_VERIFY_EMAIL === 'true') ? true : false) as boolean
 }
-
-//urlPath `${ENDPOINT}/auth/realms/${REALM}/protocol/openid-connect/userinfo`
 
 export const URLPath = {
   USER_INFO: `${APIEndpoints.AUTH}/auth/realms/${Keycloak.REALM}/protocol/openid-connect/userinfo`,
