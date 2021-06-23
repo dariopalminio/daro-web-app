c4725f63f7e0d12046ef7082ae9b97b9ccb68c7b
Daro <dario.palminio@gmail.com>
2021-06-22 22:21:12 -0400

    Comment test for useLogin is bad practice

--- client/src/__tests__/state/hook/useLogin.test.ts
+++ client/src/__tests__/state/hook/useLogin.test.ts
@@ -2,29 +2,25 @@ require('@testing-library/react');
 import { renderHook, act } from '@testing-library/react-hooks';
 import useLogin from "../../../state/hook/useLogin";
 import { AuthServiceFactory } from '../../../origin/client/user/AuthServiceFactory';
+import  AuthFakeServiceImpl  from '../../../origin/client/user/AuthFakeServiceImpl';
 import { IAuthService } from '../../../state/client/IAuthService';
 
 
 const userOk = "ok";
 const passOk = "ok";
 
-/* First: mock service for authenticate user and pass
-jest.mock("../../../origin/client/user/LoginService", () => {
-    return async function loginService(user: string, pass: string): Promise<any> {
-        return await new Promise<any>(
-            function (resolve, reject) {
-                if ((user == userOk) && (pass == passOk)) {
-                    let jwtExample = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJ0b3B0YWwuY29tIiwiZXhwIjoxNDI2NDIwODAwLCJodHRwOi8vdG9wdGFsLmNvbS9qd3RfY2xhaW1zL2lzX2FkbWluIjp0cnVlLCJjb21wYW55IjoiVG9wdGFsIiwiYXdlc29tZSI6dHJ1ZX0.yRQYnWzskCZUxPwaQupWkiUzKELZ49eM7oWxAQK_ZXw';
-                    resolve(jwtExample); // fulfilled
-                } else {
-                    var reason = new Error('Response is NOT OK. Could not authenticate!');
-                    reject(reason); // reject
-                }
-            }
-        );
+export class AuthServiceFactoryForTest {
+    static create(fake: boolean): IAuthService{
+        //Return a factory function
+        return AuthFakeServiceImpl(); // Fake
     }
-});
-*/
+};
+
+// First: mock service for authenticate user and pass
+//jest.mock("../../../origin/client/user/AuthServiceFactory", () => {
+//    return AuthServiceFactoryForTest
+//});
+
 /*
 // Second: mock service for retrieve user information
 jest.mock("../../../client/user/GetUserInfoService", () => {
@@ -70,9 +66,9 @@ describe('Test useUser Hook', () => {
     test('login with credentials OK should be SUCCESSFUL', async () => {
         //const { result, waitForNextUpdate } = renderHook(() => useLogin());
 
-        //act(() => {
+       // act(() => {
         //    result.current.login(userOk, passOk);
-        //})
+       // })
 
         //await waitForNextUpdate();
         //expect(result.current.isLoggedOk).toBe(true);
