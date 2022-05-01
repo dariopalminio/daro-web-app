7fcd5a13e88e2cb74c289595ab16157d8cc5c1e9
Daro <dario.palminio@gmail.com>
2022-05-01 15:04:26 -0400

    change: add dto.validator super class

---
+++ server/src/domain/validator/dto.validator.ts
@@ -0,0 +1,44 @@
+import Ajv from "ajv"; //https://ajv.js.org/guide/getting-started.html
+import { Type } from "@sinclair/typebox";
+import addErrors from "ajv-errors";
+import addFormats from "ajv-formats";
+import { ITranslator } from '../../domain/output-port/translator.interface';
+
+/**
+ * Ensure your data is valid using an JSON Schema or JSON Type Definition
+ */
+export class DTOValidator {
+  
+  dtoSchema = null;
+  ajvalidator = null;
+  validateDTOSchema = null;
+
+  constructor() { 
+    this.ajvalidator = new Ajv({ allErrors: true });
+    addFormats(this.ajvalidator, ["email"]).addKeyword("kind").addKeyword("modifier");
+    addErrors(this.ajvalidator, { keepErrors: false });
+    //this.dtoSchema = Type.Object(..)
+    //this.validateDTOSchema = this.ajvalidator.compile(this.dtoSchema);
+  };
+
+  async traslateValidateErrorsText(i18n?: ITranslator) {
+    let compoundMessage = '';
+
+    if (typeof(i18n) === 'undefined') {
+      compoundMessage = this.ajvalidator.errorsText(this.validateDTOSchema.errors, { separator: "\n" });
+      return compoundMessage;
+    }
+
+    for (var valor of this.validateDTOSchema.errors) {
+      compoundMessage = compoundMessage + await i18n.translate(valor.message) + "\n";
+    }
+
+    return compoundMessage;
+  };
+
+  validate(objectJson: any){
+    return this.validateDTOSchema(objectJson);
+  };
+
+
+};
--- server/src/domain/validator/login-form-dto.validator.ts
+++ server/src/domain/validator/login-form-dto.validator.ts
@@ -3,58 +3,37 @@ import { Type } from "@sinclair/typebox";
 import addErrors from "ajv-errors";
 import addFormats from "ajv-formats";
 import { ITranslator } from '../../domain/output-port/translator.interface';
+import { DTOValidator } from '../../domain/validator/dto.validator';
 
-export class LoginFormDTOValidator {
+
+export class LoginFormDTOValidator extends DTOValidator {
   
-   loginFormDTOSchema = Type.Object(
-    {
-        username: Type.String({
-            format: "email",
-                errorMessage: {
-                type: "auth.ERROR.INVALID_EMPTY_CREDENTIALS", //Debe ser un string
-                format: "auth.ERROR.INVALID_EMPTY_CREDENTIALS", //Debe ser un correo electrónico válido
-        },
-      }),
-        password: Type.String({
-            errorMessage: {
-            type: "auth.ERROR.INVALID_EMPTY_CREDENTIALS", //Debe ser un string
-        },
-      }),
-    },
-    {
-      additionalProperties: false,
-      errorMessage: {
-        additionalProperties: "auth.ERROR.INVALID_FORMAT_OBJECT", //El formato del objeto no es válido
-      },
-    }
-  );
-  ajvalidator = null;
-  validateLoginFormDTOSchema = null;
-
   constructor() { 
-    this.ajvalidator = new Ajv({ allErrors: true });
-    addFormats(this.ajvalidator, ["email"]).addKeyword("kind").addKeyword("modifier");
-    addErrors(this.ajvalidator, { keepErrors: false });
-    this.validateLoginFormDTOSchema = this.ajvalidator.compile(this.loginFormDTOSchema);
-  };
+    super();
 
-  async traslateValidateErrorsText(i18n?: ITranslator) {
-    let compoundMessage = '';
-
-    if (typeof(i18n) === 'undefined') {
-      compoundMessage = this.ajvalidator.errorsText(this.validateLoginFormDTOSchema.errors, { separator: "\n" });
-      return compoundMessage;
-    }
-
-    for (var valor of this.validateLoginFormDTOSchema.errors) {
-      compoundMessage = compoundMessage + await i18n.translate(valor.message) + "\n";
-    }
-
-    return compoundMessage;
-  };
-
-  validate(objectJson: any){
-    return this.validateLoginFormDTOSchema(objectJson);
+    this.dtoSchema = Type.Object(
+      {
+          username: Type.String({
+              format: "email",
+                  errorMessage: {
+                  type: "auth.ERROR.INVALID_EMPTY_CREDENTIALS", //Debe ser un string
+                  format: "auth.ERROR.INVALID_EMPTY_CREDENTIALS", //Debe ser un correo electrónico válido
+          },
+        }),
+          password: Type.String({
+              errorMessage: {
+              type: "auth.ERROR.INVALID_EMPTY_CREDENTIALS", //Debe ser un string
+          },
+        }),
+      },
+      {
+        additionalProperties: false,
+        errorMessage: {
+          additionalProperties: "auth.ERROR.INVALID_FORMAT_OBJECT", //El formato del objeto no es válido
+        },
+      }
+    );
+    this.validateDTOSchema = this.ajvalidator.compile(this.dtoSchema);
   };
 
 
